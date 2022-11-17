b984d9274865bb53ed22d99bfa05dce16811c053
Muhammad Fadilah Nurjaman <96771710+dhilz202@users.noreply.github.com>
2022-11-17 13:31:23 +0700

    Add files via upload

---
+++ UTS/PBO/BangunDatar.java
@@ -0,0 +1,7 @@
+package UTS.PBO;
+
+public interface BangunDatar {
+    void luas();
+
+    double getLuas();
+}
---
+++ UTS/PBO/PersegiPanjang.java
@@ -0,0 +1,23 @@
+package UTS.com;
+
+public class PersegiPanjang implements BangunDatar {
+    double panjang;
+    double lebar;
+    double luasPersegiPanjang;
+
+    public PersegiPanjang(double panjang, double lebar) {
+        this.panjang = panjang;
+        this.lebar = lebar;
+    }
+
+    @Override
+    public void luas() {
+        luasPersegiPanjang = this.panjang * this.lebar;
+    }
+
+    @Override
+    public double getLuas() {
+        return luasPersegiPanjang;
+    }
+
+}
